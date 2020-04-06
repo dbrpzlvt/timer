70c7ff8af6b86f21194e30f0bb71795b780ec53b
Бевз Роман Юрьевич <bevz.riu@students.dvfu.ru>
2020-04-06 21:45:15 +1000

    Добавьте файлы проекта.

---
+++ timer/Form1.Designer.cs
@@ -0,0 +1,177 @@
+﻿namespace timer
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// Обязательная переменная конструктора.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Освободить все используемые ресурсы.
+		/// </summary>
+		/// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Код, автоматически созданный конструктором форм Windows
+
+		/// <summary>
+		/// Требуемый метод для поддержки конструктора — не изменяйте 
+		/// содержимое этого метода с помощью редактора кода.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.components = new System.ComponentModel.Container();
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+			this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+			this.timer1 = new System.Windows.Forms.Timer(this.components);
+			this.pictureBox2 = new System.Windows.Forms.PictureBox();
+			this.pictureBox1 = new System.Windows.Forms.PictureBox();
+			this.button1 = new System.Windows.Forms.Button();
+			this.richTextBox2 = new System.Windows.Forms.RichTextBox();
+			this.label1 = new System.Windows.Forms.Label();
+			this.label2 = new System.Windows.Forms.Label();
+			this.button2 = new System.Windows.Forms.Button();
+			this.groupBox1 = new System.Windows.Forms.GroupBox();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+			this.groupBox1.SuspendLayout();
+			this.SuspendLayout();
+			// 
+			// richTextBox1
+			// 
+			this.richTextBox1.Location = new System.Drawing.Point(6, 52);
+			this.richTextBox1.Name = "richTextBox1";
+			this.richTextBox1.Size = new System.Drawing.Size(177, 257);
+			this.richTextBox1.TabIndex = 3;
+			this.richTextBox1.Text = "";
+			// 
+			// timer1
+			// 
+			this.timer1.Enabled = true;
+			this.timer1.Interval = 2000;
+			this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+			// 
+			// pictureBox2
+			// 
+			this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
+			this.pictureBox2.Location = new System.Drawing.Point(12, 277);
+			this.pictureBox2.Name = "pictureBox2";
+			this.pictureBox2.Size = new System.Drawing.Size(405, 157);
+			this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+			this.pictureBox2.TabIndex = 1;
+			this.pictureBox2.TabStop = false;
+			// 
+			// pictureBox1
+			// 
+			this.pictureBox1.Image = global::timer.Properties.Resources.ff08c7cf7604a32b2b952327b9caf26d1;
+			this.pictureBox1.Location = new System.Drawing.Point(127, 12);
+			this.pictureBox1.Name = "pictureBox1";
+			this.pictureBox1.Size = new System.Drawing.Size(200, 239);
+			this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+			this.pictureBox1.TabIndex = 0;
+			this.pictureBox1.TabStop = false;
+			// 
+			// button1
+			// 
+			this.button1.Location = new System.Drawing.Point(53, 340);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(112, 41);
+			this.button1.TabIndex = 2;
+			this.button1.Text = "Остановить";
+			this.button1.UseVisualStyleBackColor = true;
+			this.button1.Click += new System.EventHandler(this.button1_Click);
+			// 
+			// richTextBox2
+			// 
+			this.richTextBox2.Location = new System.Drawing.Point(195, 52);
+			this.richTextBox2.Name = "richTextBox2";
+			this.richTextBox2.Size = new System.Drawing.Size(177, 257);
+			this.richTextBox2.TabIndex = 4;
+			this.richTextBox2.Text = "";
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Location = new System.Drawing.Point(3, 32);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(162, 17);
+			this.label1.TabIndex = 5;
+			this.label1.Text = "Центральная картинка";
+			// 
+			// label2
+			// 
+			this.label2.AutoSize = true;
+			this.label2.Location = new System.Drawing.Point(192, 32);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(124, 17);
+			this.label2.TabIndex = 6;
+			this.label2.Text = "Нижняя картинка";
+			// 
+			// button2
+			// 
+			this.button2.Location = new System.Drawing.Point(225, 340);
+			this.button2.Name = "button2";
+			this.button2.Size = new System.Drawing.Size(112, 41);
+			this.button2.TabIndex = 7;
+			this.button2.Text = "Продолжить";
+			this.button2.UseVisualStyleBackColor = true;
+			this.button2.Click += new System.EventHandler(this.button2_Click);
+			// 
+			// groupBox1
+			// 
+			this.groupBox1.Controls.Add(this.button2);
+			this.groupBox1.Controls.Add(this.label2);
+			this.groupBox1.Controls.Add(this.label1);
+			this.groupBox1.Controls.Add(this.richTextBox2);
+			this.groupBox1.Controls.Add(this.richTextBox1);
+			this.groupBox1.Controls.Add(this.button1);
+			this.groupBox1.Location = new System.Drawing.Point(423, 13);
+			this.groupBox1.Name = "groupBox1";
+			this.groupBox1.Size = new System.Drawing.Size(378, 429);
+			this.groupBox1.TabIndex = 8;
+			this.groupBox1.TabStop = false;
+			this.groupBox1.Text = "Рабочая обрасть";
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(805, 446);
+			this.Controls.Add(this.groupBox1);
+			this.Controls.Add(this.pictureBox2);
+			this.Controls.Add(this.pictureBox1);
+			this.Name = "Form1";
+			this.Text = "Form1";
+			this.Load += new System.EventHandler(this.Form1_Load);
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+			this.groupBox1.ResumeLayout(false);
+			this.groupBox1.PerformLayout();
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.PictureBox pictureBox1;
+		private System.Windows.Forms.PictureBox pictureBox2;
+		private System.Windows.Forms.RichTextBox richTextBox1;
+		private System.Windows.Forms.Timer timer1;
+		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.RichTextBox richTextBox2;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.Button button2;
+		private System.Windows.Forms.GroupBox groupBox1;
+	}
+}
+
---
+++ timer/Form1.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace timer
+{
+	public partial class Form1 : Form
+	{
+		public Form1()
+		{
+			InitializeComponent();
+		}
+
+		System.Threading.Timer timerpicbox1;
+		System.Threading.Timer timerpicbox2;
+
+		private void button1_Click(object sender, EventArgs e)
+		{
+			timerpicbox1.Dispose();
+			timerpicbox2.Dispose();
+			richTextBox1.AppendText("Остановка работы /n");
+			richTextBox2.AppendText("Остановка работы /n");
+		}
+
+		private void Form1_Load(object sender, EventArgs e)
+		{
+			//попробуем потоки...
+			//первая картинка
+			TimerCallback datawrite = new TimerCallback(DataWrite);
+			string data = "";
+
+			//вторая картинка
+			TimerCallback datawrite1 = new TimerCallback(DataWrite1);
+			string data1 = "";
+
+			//таймер timerpicbox1 создает новый поток для datawrite
+			timerpicbox1 = new System.Threading.Timer(datawrite, data, 0, 2000);
+
+			//таймер timerpicbox2 создает второй поток для datawrite2
+			timerpicbox2 = new System.Threading.Timer(datawrite1, data1, 0, 5000);
+
+			//классический формовский таймер, думал сделать через него, но не вышло
+			//System.Windows.Forms.Timer tmr = new System.Windows.Forms.Timer();
+			//tmr.Interval = 2000;
+			//tmr.Tick += timer1_Tick;
+			//tmr.Start();
+		}
+
+		public delegate void AddMessageDelegate(string message);
+
+		public void DataWrite(object data)
+		{
+			Invoke(new AddMessageDelegate(firstpicbox), new object[] { data });
+		}
+		public void DataWrite1(object data)
+		{
+			Invoke(new AddMessageDelegate(secondpicbox), new object[] { data });
+		}
+		public void firstpicbox(object data)
+		{
+			pictureBox1.Visible = !pictureBox1.Visible;
+
+			//это для нумерации строк в textbox'e... забей...
+			//string[] buf = richTextBox1.Lines;
+			//for (int i = 0; i < richTextBox1.Lines.Length; i++)
+			//{
+			//	buf[i] = (i).ToString() + ". " + buf[i];
+			//}
+			//richTextBox1.Lines = buf;
+
+			richTextBox1.AppendText(">>> " + DateTime.Now.ToString("h:mm:ss.fff") + "\n");
+		}
+
+		public void secondpicbox(object data)
+		{
+			if (!pictureBox1.Visible)
+			{
+				pictureBox2.Visible = !pictureBox2.Visible;
+				richTextBox2.AppendText(">>> " + DateTime.Now.ToString("h:mm:ss.fff") + "\n");
+			}
+			else
+			{
+				pictureBox2.Visible = !pictureBox2.Visible;
+				richTextBox2.AppendText(">>> " + DateTime.Now.ToString("h:mm:ss.fff") + "\n");
+			}
+		}
+
+		private void timer1_Tick(object sender, EventArgs e)
+		{
+			//pictureBox1.BackColor = pictureBox1.BackColor == Color.Red ? Color.FromName("Control") : Color.Red;
+		}
+
+		private void button2_Click(object sender, EventArgs e)
+		{
+			Form1_Load(sender, e);
+		}
+
+		//public void kostil(object data)
+		//{
+		//	pictureBox1.Visible = false;
+		//}
+	}
+}
